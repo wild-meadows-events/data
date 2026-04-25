a94e8a83c18d23439d22ff1ceccf9b2e64f66950
Nathan Blair <me@nathanblair.rocks>
2026-04-25 09:35:22 -0400

    fix: use zod validation in schema

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 export { SafeIdentifier } from "./identifier.ts"
-export { DEFAULT_MIGRATIONS_TABLE, DEFAULT_STATEMENT_SEPARATOR, migrate } from "./migrate.ts"
-export type { Migration, MigrationResult } from "./types.ts"
+
+export {
+  DEFAULT_MIGRATIONS_TABLE,
+  DEFAULT_STATEMENT_SEPARATOR,
+  migrate,
+} from "./migrate.ts"
+
+export {
+  type Migration,
+  type MigrationResult,
+  MigrationResultEntrySchema,
+  MigrationResultSchema,
+} from "./types.ts"
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,18 @@
 import type { D1Result } from "@cloudflare/workers-types"
+import { z } from "zod"
 
 export type Migration = {
   name: string
   sql: string
 }
+
+export const MigrationResultEntrySchema = z.object({
+  name: z.string(),
+  results: z.array(z.unknown()),
+  skipped: z.boolean(),
+})
+
+export const MigrationResultSchema = z.array(MigrationResultEntrySchema)
 
 export type MigrationResultEntry = {
   name: string
